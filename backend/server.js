d88321fbcb59720ccc6f0728732111919b70b371
Vincent <vincent.goguet@vooler.fi>
2020-12-08 17:25:36 +0200

    Draft Frontend : MOCK in progress Backend : Auth in progress   working functions:     - /login     - /register     - /logout

--- backend/server.js
+++ backend/server.js
@@ -1,107 +1,265 @@
+
+/**
+ * Includes
+ */
 const express = require("express");
 const bodyParser = require("body-parser");
 
-let app = express(); //initialise
+//const apiroutes = require("./routes/apiroutes"); //all routes for the application
+
+const crypto = require('crypto');
+//const secret = 'thisissecret';
+const secret = 'Hac in hora Sine mora Corde pulsum tangite Quod per sortem Sternit forterm Mecum omnes plangite'
+const algorithm = 'sha256';
+
+/**
+ * Globales values
+ */
 let LISTENING_PORT = 3101;
-let ROOL_PATH_LISTENING = "/api/accountmanager"
+let PASSWORD_MIN_LENGTH = 14;
+let EMAIL_MIN_LENGTH = 5;
+
+//session stuff:
+const ttl_diff = 36000000; //ttl = time to live (how long the session will stay) - here 1h
+
 
 let id = 100;
 
-//used in the folowing order:
+let app = express(); //initialise
+
 app.use(bodyParser.json());
 
 
-//DATABASE
-let database = [];
-//let id = 100;
+
 
 /**
- * GET 
- *  Handle get request.
- *  Send the full data.
+ * DATABASES
  */
-app.get(ROOL_PATH_LISTENING, function(req, res){ //get command with request, respond
-    console.log(getTime() + " Get command received on " + ROOL_PATH_LISTENING);
-    return res.status(200).json(database);
-});
+let database = [];          //list of data
+let registeredUsers = [];   //list of users available for the application
+let loggedSession = [];     //array of live sessions.   
+
+
+
+
 
 /**
- * POST 
- *  Store the received data
+ * MIDDLEWARE
  */
-app.post(ROOL_PATH_LISTENING, function(req, res){
-    console.log(getTime() + " Post command received on " + ROOL_PATH_LISTENING);
-    let item = {
-        id:id,
-        dateAction:req.body.dateAction,
-        label:req.body.label,
-        amount:req.body.amount,
-        dateImpactOnBank:req.body.dateImpactOnBank,
-        accountId:req.body.accountId
+//Generate hash for encryption
+function getHash(text) {
+    const hash = crypto.createHmac(algorithm, secret)
+    .update(text)
+    .digest('hex');
+    return hash;
+}
+
+createToken = () => {
+    let token = "";
+    let letters = "ABCDEFGHIJabcdefghij0123456789" //no car used in URL
+    for (let i=0; i<256; i++) {
+        let temp = Math.floor(Math.random()*letters.length);
+        token = token + letters[temp];
     }
-    console.log(getTime() + " Data to insert :");
-    console.log(getTime() + " \tId              : " + item.id);
-    console.log(getTime() + " \tDateAction      : " + item.dateAction);
-    console.log(getTime() + " \tLabel           : " + item.label);
-    console.log(getTime() + " \tAmount          : " + item.amount);
-    console.log(getTime() + " \tdateImpactOnBank: " + item.dateImpactOnBank);
-    console.log(getTime() + " \taccountId       : " + item.accountId);
-    id++;
-    database.push(item);
-    return res.status(200).json({message:"success"});
-});
-
-/**
- * DELETE
- *  Suppress record based on the id
- */
-app.delete(ROOL_PATH_LISTENING+"/:id", function(req, res){
-    console.log(getTime() + " Delete command received on " + ROOL_PATH_LISTENING);
-    console.log(getTime() + " Id to delete: " + req.params.id);
-    let id = parseInt(req.params.id, 10);
-    let tempDatabase = database.filter(item => item.id !== id);
-    database = tempDatabase;
-    return res.status(200).json({message:"suppress success"});
-});
-
+    return token;
+}
 
 
 
 /**
- * PUT
- *  update of a record based on the id
+ * FUNCTIONS
  */
-app.put(ROOL_PATH_LISTENING+"/:id", function(req, res){
-    console.log(getTime() + " Put command received on " + ROOL_PATH_LISTENING);
-    let tmp_id = parseInt(req.params.id, 10);
-    console.log(getTime() + " Id to update : " + tmp_id);
-    let item = {
-        id:req.body.id,
-        dateAction:req.body.dateAction,
-        label:req.body.label,
-        amount:req.body.amount,
-        dateImpactOnBank:req.body.dateImpactOnBank,
-        accountId:req.body.accountId
+
+/**
+ * checkRequestCorps
+ * 
+ *  The function will check :
+ *      - if the body is present
+ *      - if the email is present and respect the minimum length
+ *      - if the password is present and respect the minimum length
+ * 
+ * @param req : the request received
+ * @return obj : return an object with:
+ *          flgIsChkOk: boolean if the check is ok.
+ *          errorMsg : error message if the check is incorect
+ */
+function checkRequestCorps(req) {
+    //Object returned by the function
+    let returnObj = {
+            flgIsChkOk:true,
+            errorMsg:""
+        }
+    
+    var message = ""; //variable used to create adaptative message
+
+    //check if data is legit:
+    if(!req.body) { 
+        returnObj.flgIsChkOk = false;
+        errorMsg = "ERROR No body in request";
+        return returnObj;
     }
-    console.log(getTime() + " Containt of the Update :");
-    console.log(getTime() + " \tDateAction      : " + item.dateAction);
-    console.log(getTime() + " \tLabel           : " + item.label);
-    console.log(getTime() + " \tAmount          : " + item.amount);
-    console.log(getTime() + " \tdateImpactOnBank: " + item.dateImpactOnBank);
-    console.log(getTime() + " \taccountId       : " + item.accountId);
 
-    for(let i=0; i<database.length; i++) {
-        if(database[i].id === tmp_id) {
-            database.splice(i, 1, item);
-            return res.status(200).json({message:"put success"});
+    //if request is incomplete:
+    if(!req.body.password || !req.body.email){
+        if (!req.body.email) message="No email ";
+        if(!req.body.password) message="No password";
+        returnObj.flgIsChkOk = false;
+        returnObj.errorMsg = "ERROR parameter incorect in req: " + message;
+        return returnObj;
+    }
+    
+    //checking size of password and email
+    if(req.body.password.length < PASSWORD_MIN_LENGTH || req.body.email.length < EMAIL_MIN_LENGTH){
+        if (req.body.password.length < PASSWORD_MIN_LENGTH) message = "Password length insufficient ";
+        if (req.body.email.length < EMAIL_MIN_LENGTH) message = "Username length insufficient ";
+        returnObj.flgIsChkOk = false;
+        returnObj.errorMsg = "ERROR parameter incorect in req: " + message;
+        return returnObj;
+    }
+
+    return returnObj;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * HANDELING AUTHENTIFICATION
+ */
+
+/**
+ * - REGISTER - 
+ *      Creating new user 
+ */
+app.post("/register", function(req, res) {
+    console.log("REGISTER request to create a new user.");
+
+    //Basic parameter check of the request (set in function)
+    let testRes = checkRequestCorps(req);
+    if(!testRes.flgIsChkOk){
+        console.error("REGISTER " + testRes.errorMsg);
+        return res.status(400).json({message:"Bad Request"});
+    }
+
+    //check if the email is available
+    for(let i=0; i<registeredUsers.length; i++) {
+        if(req.body.email === registeredUsers[i].email) {
+            console.error("REGISTER ERROR Registration failed : Username is already use");
+            return res.status(409).json({message:"Bad request : Username is already use"});
         }
     }
-    return res.status(404).json({message:"put not found"});
-})
+
+    //Generating Hash form password and storing it.
+    myHash = getHash(req.body.password);
+    console.log("REGISTER hash genetared: " + myHash);
+    let user = {
+        email:req.body.email,
+        password:myHash
+    }
+    registeredUsers.push(user);
+    console.log("REGISTER Registration of new user " + user.email + " succeed");
+    return res.status(200).json({message:"success"});     
+});
 
 
+/**
+ * - LOGIN -  
+ *      Session creation
+ */
+app.post("/login", function(req, res) {
+    console.log("LOGIN requested");
+    //Basic parameter check of the request (set in function)
+    let testRes = checkRequestCorps(req);
+    if(!testRes.flgIsChkOk){
+        console.error("REGISTER " + testRes.errorMsg);
+        return res.status(400).json({message:"Bad Request"});
+    }
+
+    console.log("LOGIN Login requested for " + req.body.email);
+
+    for(let i=0; i<registeredUsers.length; i++) {
+        if(registeredUsers[i].email === req.body.email) {
+            console.log("LOGIN User "+  req.body.email +" is available for the application");
+            myHash=getHash(req.body.password);
+            //console.log("LOGIN : hash stored for the user : " + registeredUsers[i].password);
+            //console.log("LOGIN : hash generated form pwd  : " + myHash);
+            if(myHash.localeCompare(registeredUsers[i].password) === 0){
+                console.log("LOGIN password confirmed");
+                let token = createToken();
+                let now = Date.now();
+                let session = {
+                    email: req.body.email,
+                    ttl: now + ttl_diff,
+                    token: token
+                }
+                loggedSession.push(session);
+                console.log("LOGIN session created : " + token);
+                return res.status(200).json({token:token});
+            }
+            else {
+                console.error("LOGIN ERROR : authentication error");
+                return res.status(403).json({message:"forbidden"});
+            }
+        }
+        console.error("LOGIN ERROR User don't exist in appilcation.")
+        return res.status(401).json({message:"User unauthorized"});
+    }
+    return res.status(403).json({message:"forbidden"});
+});
+
+
+
+/**
+ * - LOGOUT -  
+ *      Closing session
+ */
+app.post("/logout", function(req, res) {
+    console.log("LOGOUT requested")
+    if(!req.headers.token){
+        console.error("LOGOUT ERROR No token sent in the requet");
+        return res.status(404).json({message:"nout found"});
+    }
+    if(registeredUsers.length>0){
+        for(let i=0; i<registeredUsers.length; i++) {
+            if(loggedSession[i].token === req.headers.token) {
+                console.log("LOGOUT user : " + loggedSession[i].email);
+                loggedSession.splice(i, 1);
+                return res.status(200).json({message:"success"});
+            }
+        }
+    }
+    else{
+        console.error("LOGOUT ERROR No user registred for the application !!");
+        return res.status(404).json({message:"not found"});
+    }
+    console.error("LOGOUT ERROR Session not found for token: " + req.header.token);
+    return res.status(404).json({message:"not found"});
+});
+
+
+
+
+
+/**
+ * Start listening
+ */
 app.listen(LISTENING_PORT);
-console.log("Running on port " + LISTENING_PORT + " on path: " + ROOL_PATH_LISTENING);
+console.log("Running on port " + LISTENING_PORT);
+
+
+/**
+ * Install moduls : 
+ * npm i aesjs
+ */
 
 /* running the server : 
  *  node server 
@@ -114,10 +272,39 @@ console.log("Running on port " + LISTENING_PORT + " on path: " + ROOL_PATH_LISTE
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log("Inserting data for tests");
 /*
  * Création d'un jeu de donnée:
  */
+/*
 function getTime() { 
     let today = new Date();
     let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -155,5 +342,5 @@ let item3 = {
     accountId:0
 }
 database.push(item3);
-
+*/
 console.log("Insert done");
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import ConnexionForm from './components/ConnexionForm';
+import NavBarForm from './components/NavBar';
+
+import HomeForm from './components/HomeForm';
+import AddActionForm from './components/ActionList/AddActionForm';
+import AddActionList from './components/ActionList/AddActionList';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list:[
+        {date:"01/12/2020", label:"list", amount:"85.26"},
+        {date:"05/12/2020", label:"petrol", amount:"59.43"},
+        {date:"08/12/2020", label:"chocolat", amount:"43.52"},
+        {date:"10/12/2020", label:"Xmas present", amount:"156.53"},
+      ],
+      lastactions:[
+        {date:"01/12/2020", label:"KMarcket", amount:"85.26"},
+        {date:"05/12/2020", label:"petrol", amount:"59.43"},
+        {date:"08/12/2020", label:"chocolat", amount:"43.52"},
+        {date:"10/12/2020", label:"Xmas present", amount:"156.53"},
+      ],
+
+      history:[
+        {date:"01/01/2018", label:"Colis EcoParcel", amount:90.20},
+        {date:"20/04/2018", label:"Posti", amount:11.80},
+        {date:"01/12/2018", label:"Plein", amount:37.03},
+        {date:"01/06/2019", label:"XXL", amount:29.80}
+      ]
+    };
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <div className="header">
+          <ConnexionForm/>
+          <NavBarForm/>
+        </div>
+
+        <div id="corps">
+        <Switch>
+          <Route exact path="/" render={ () => (<HomeForm/>)} />
+          <Route exact path="/lastaction" render={ () => (<HomeForm/>)} />
+          <Route exact path="/addaction" render={ () => (<AddActionForm list={this.state.list}/>)} />
+          <Route exact path="/lastactions" render={ () => (<AddActionList list={this.state.lastactions}/>)} />
+          <Route exact path="/history" render={ () => (<AddActionList list={this.state.history}  />)} />
+        </Switch>
+        </div>
+      </div>      
+    );
+  }
 }
 
 export default App;
